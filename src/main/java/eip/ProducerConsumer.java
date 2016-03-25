9e19fd10a3f06629e6bd817bc0eeaf1d17ceba7c
valentinCro <valentin.crochemore1@etu.univ-rouen.fr>
2016-03-25 14:11:40 +0100

    feat () : add ProducerConsumer.java

---
+++ src/main/java/eip/ProducerConsumer.java
@@ -0,0 +1,38 @@
+package eip;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.log4j.BasicConfigurator;
+
+public class ProducerConsumer {
+    public static void main (String [] args) throws Exception {
+        //Configure le logger par défaut
+        BasicConfigurator.configure();
+
+        //Contexte Camel par défaut
+        CamelContext context = new DefaultCamelContext();
+
+        //Crée une route contenant le consommateur
+        RouteBuilder routeBuilder = new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                //On définit un consommateur 'consumer-1'
+                //qui va écrire le message
+                from("direct:consumer-1").to("log:afficher-1-log");
+            }
+        };
+
+        //on ajoute la route au contexte
+        routeBuilder.addRoutesToCamelContext(context);
+
+        //On démarre le contexte pour activer les routes
+        context.start();
+
+        //On créé un producteur
+        ProducerTemplate pt = context.createProducerTemplate();
+        //qui envoie un message au consommateur 'consumer-1'
+        pt.sendBody("direct:consumer-1", "Hello world Buddy !");
+    }
+}
